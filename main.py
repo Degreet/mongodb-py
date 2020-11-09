4fe12f6905055b42bcce086d20ea928067f63e6b
Degreet <degreetpro@gmail.com>
2020-11-09 11:01:08 +0200

    reg user

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from pymongo import MongoClient
-import config
+import config, random
 
 cluster = MongoClient(config.uri)
 db = cluster.mongodb_py
+
+def reg():
+  name = input("Придумайте логин >> ")
+  user = db.users.find_one({ "name": name })
+
+  if user:
+    print("Ошибка. Такой логин уже занят.\n")
+    reg()
+  else:
+    code = random.randint(100, 900)
+    db.users.insert_one({ "name": name, "code": code })
+    print("Вы были зарегистрированы! Ваш код: " + str(code))
+
+reg()
